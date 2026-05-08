7cc349f6bb5010d0477b40663d3130557c779eae
Kumar Madhappa <kumar.m.1987@gmail.com>
2026-05-08 09:57:21 +0800

    Adding method to get only one movie based on imdbid

--- src/main/java/com/kumar/movies/MovieService.java
+++ src/main/java/com/kumar/movies/MovieService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -13,5 +14,9 @@ public class MovieService {
 
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovieByImdbId(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
